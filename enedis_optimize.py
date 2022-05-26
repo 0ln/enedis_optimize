62fc21ee300198d825cfae1e65fe408f269547e8
0ln <8427756+0ln@users.noreply.github.com>
2022-05-27 00:48:49 +0200

    Fix initial bugs

--- enedis_optimize.py
+++ enedis_optimize.py
@@ -12,24 +12,26 @@ lows = [
 
 print("Enedis Optimize\n")
 
-data = [i.strip("\n").split(";") for i in fileinput.input()]
+data = [i.strip("\n").split(";") for i in fileinput.input() if i[-2:] != ";\n"]
 del data[:data.index(["Horodate", "Valeur"]) + 1]
 data = [(dt.datetime.fromisoformat(i[0]), int(i[1])) for i in data]
 
 cost = {"standard": {}, "lows": [{},] * len(lows)}
 for i in data: cost["standard"][i[0]] = i[1] * kWh["standard"] / 1000
-for i in range(len(lows)):
+delta_total = dt.timedelta()
+for i in enumerate(lows):
     for j in enumerate(data):
-        if i > 0: delta = abs(j[1][0] - data[j[0] - 1][0])
-        else: delta = abs(j[1][0] - data[j[0] + 1][0])
-        cost["lows"][i][j[1][0]] = j[1][1] * kWh.values()[1:][any(k[0] <= j[1][0] - delta < k[1] for k in j)] / 1000
+        delta = abs(j[1][0] - data[j[0] - (1 if j[0] > 0 else -1)][0])
+        if delta > dt.timedelta(hours = 1): delta = abs(j[1][0] - data[j[0] - (1 if j[0] == len(data) - 1 else - 1)][0])
+        cost["lows"][i[0]][j[1][0]] = j[1][1] * list(kWh.values())[1:][any(k[0] <= (j[1][0] - delta).hour < k[1] for k in i[1])] / 1000
+        delta_total += delta
 
-delta = (data[-1][0] - data[0][0]) / dt.timedelta(days = 365.2425 / 12)
+delta_total /= dt.timedelta(days = 365.2425 / 12)
 print("Costs:")
-print("\tStandard:", round((monthly_standard := sum(cost["standard"].values())) / delta, 2), "monthly")
+print("\tStandard:", "{0:.02f}".format((monthly_standard := sum(cost["standard"].values())) / delta_total), "monthly")
 print("\tLows:")
-for i in enumerate(cost["lows"]): print("\t\t" + str(lows[i[0]]) + ":", round(sum(i[1].values()) / delta, 2), "monthly")
-cost["lows"] = {i[0]: st.fmean([cost["lows"][j][i] for j in range(len(lows))]) for i in data}
-print("\t\tAverage:", round((monthly_lows := sum(cost["lows"].values())) / delta, 2), "monthly")
-print("\tDifference:", "{0:+.02f}".format((monthly_lows - monthly_standard) / delta), "monthly")
+for i in enumerate(cost["lows"]): print("\t\t" + str(lows[i[0]]) + ":", "{0:.02f}".format(sum(i[1].values()) / delta_total), "monthly")
+cost["lows"] = {i[0]: st.fmean([cost["lows"][j][i[0]] for j in range(len(lows))]) for i in data}
+print("\t\tAverage:", "{0:.02f}".format((monthly_lows := sum(cost["lows"].values())) / delta_total), "monthly")
+print("\tDifference:", "{0:+.02f}".format((monthly_lows - monthly_standard) / delta_total), "monthly")
 print()
