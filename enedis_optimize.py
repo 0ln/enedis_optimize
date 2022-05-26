06ad85c56892744c8e6bb34c4467108cef0c7bb6
0ln <8427756+0ln@users.noreply.github.com>
2022-05-26 23:19:52 +0200

    Add cost calculation

--- enedis_optimize.py
+++ enedis_optimize.py
@@ -2,7 +2,7 @@
 
 import fileinput, datetime as dt
 
-kWh = {"standard": 0.174, "low": 0.147, "high": 0.1841}
+kWh = {"standard": .174, "low": .147, "high": .1841}
 lows = [
     [(2, 7), (12.5, 15.5)],
     [(3, 8), (13.5, 16.5)],
@@ -14,4 +14,10 @@ data = [i.strip("\n").split(";") for i in fileinput.input()]
 del data[:data.index(["Horodate", "Valeur"]) + 1]
 data = [(dt.datetime.fromisoformat(i[0]), int(i[1])) for i in data]
 
-cost = {"standard": [], "lows": []}
+cost = {"standard": [], "lows": [[],] * len(lows)}
+for i in data: cost["standard"].append((i[0], i[1] * kWh["standard"] / 1000))
+for i in range(len(lows)):
+    for j in enumerate(data):
+        if i > 0: delta = abs(j[1][0] - data[j[0] - 1][0])
+        else: delta = abs(j[1][0] - data[j[0] + 1][0])
+        cost["lows"][i].append((j[1][0], j[1][1] * kWh.values()[1:][any(k[0] <= j[1][0] - delta < k[1] for k in j)] / 1000))
