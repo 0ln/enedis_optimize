91d6ede2edc3c4dced0acd364c17728b8a57bccd
0ln <8427756+0ln@users.noreply.github.com>
2022-05-26 22:14:56 +0200

    Add time parsing

--- enedis_optimize.py
+++ enedis_optimize.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-import fileinput
+import fileinput, datetime
 
-f = [i.strip("\n") for i in fileinput.input()]
-del f[:f.index("Horodate;Valeur") + 1]
+data = [i.strip("\n").split(";") for i in fileinput.input()]
+del data[:data.index(["Horodate", "Valeur"]) + 1]
+data = [(datetime.fromisoformat(i[0]), int(i[1])) for i in data]
