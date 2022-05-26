ec3ed6ffaadba0e97fcffe8bed685b48c68433b3
0ln <8427756+0ln@users.noreply.github.com>
2022-05-27 01:43:33 +0200

    Fix delta issue

--- enedis_optimize.py
+++ enedis_optimize.py
@@ -10,23 +10,24 @@ lows = [
     [(0, 6.5), (22.5, 24)]
 ]
 
-print("Enedis Optimize\n")
+print("Enedis Optimize")
+print()
 
 data = [i.strip("\n").split(";") for i in fileinput.input() if i[-2:] != ";\n"]
 del data[:data.index(["Horodate", "Valeur"]) + 1]
 data = [(dt.datetime.fromisoformat(i[0]), int(i[1])) for i in data]
 
 cost = {"standard": {}, "lows": [{},] * len(lows)}
-for i in data: cost["standard"][i[0]] = i[1] * kWh["standard"] / 1000
 delta_total = dt.timedelta()
 for i in enumerate(lows):
     for j in enumerate(data):
         delta = abs(j[1][0] - data[j[0] - (1 if j[0] > 0 else -1)][0])
         if delta > dt.timedelta(hours = 1):
             delta_total -= delta
-            delta = abs(j[1][0] - data[j[0] - (1 if j[0] == len(data) - 1 else - 1)][0])
+            delta = abs(j[1][0] - data[j[0] - (1 if j[0] == len(data) - 1 else -1)][0])
             delta_total += delta
-        cost["lows"][i[0]][j[1][0]] = j[1][1] * list(kWh.values())[1:][any(k[0] <= (j[1][0] - delta).hour < k[1] for k in i[1])] / 1000
+        cost["standard"][j[1][0]] = j[1][1] * kWh["standard"] / 1000 * delta / dt.timedelta(hours = 1)
+        cost["lows"][i[0]][j[1][0]] = j[1][1] * list(kWh.values())[1:][any(k[0] <= (j[1][0] - delta).hour < k[1] for k in i[1])] / 1000 * delta / dt.timedelta(hours = 1)
 
 delta_total = (delta_total + data[-1][0] - data[0][0]) / dt.timedelta(days = 365.2425 / 12)
 print("Costs:")
